19877e6f73d062e32589623e4bc4dfd781b9dc44
ArianeCMachado <109089136+ArianeCMachado@users.noreply.github.com>
2022-07-20 17:29:34 -0300

    certo

--- src/cipher.js
+++ src/cipher.js
@@ -9,16 +9,3 @@ functioncifra(String){return
 
 export default cipher;
 
-function prev() {
-  if(key > 1) {
-      key--;
-      document.getElementById("key").innerHTML = key;
-    }
-}
-
-function next() {
-    if(key < 25) {
-        key++;
-        document.getElementById("key").innerHTML = key;
-    }
-}
