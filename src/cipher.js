e852ae75996f908a73fafa6cc0669d11749f5264
ArianeCMachado <109089136+ArianeCMachado@users.noreply.github.com>
2022-07-20 17:06:11 -0300

    html e css começo

--- src/cipher.js
+++ src/cipher.js
@@ -2,4 +2,23 @@ const cipher = {
   // ...
 };
 
+functioncifra(String){return 
+}
+
+
+
 export default cipher;
+
+function prev() {
+  if(key > 1) {
+      key--;
+      document.getElementById("key").innerHTML = key;
+    }
+}
+
+function next() {
+    if(key < 25) {
+        key++;
+        document.getElementById("key").innerHTML = key;
+    }
+}
