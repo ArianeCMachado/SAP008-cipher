6241728658339ed1f0d1b8d9bbe2394e265ba74c
ArianeCMachado <109089136+ArianeCMachado@users.noreply.github.com>
2022-08-04 13:40:01 -0300

    JS funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,32 @@
+//encode:
+const encode = function (palavra, deslocamento) {
+    var output = "";
+    for (var i = 0; i < palavra.length; i++) {
+        var atual = palavra[i];
+        console.log(atual)
+        var code = palavra.charCodeAt(i);
+        console.log(code)
+        if (code >= 65 && code <= 90) {
+            atual = String.fromCharCode(((code - 65 + deslocamento) % 26) + 65);
+            console.log(atual)
+            console.log((((code - 65 + deslocamento) % 26) + 65))
+        }
+ 
+        else if (code >= 97 && code <= 122) {
+        atual = String.fromCharCode(((code - 97 + deslocamento) % 26) + 97);
+        }
+        output += atual;
+    }
+   
+    console.log(output)
+    console.log(atual)
+    return output;
+};
+
+const cipher = {encode};
+
+//decode
 
 
-const encode = function (str, amount) {
-    if (amount < 0) {
-     return encode(str, amount + 26);
-   }
- 
-     var output = "";
- 
-    for (var i = 0; i < str.length; i++) {
-        var c = str[i];
- 
-       if (c.match(/[a-z]/i)) {
-            var code = str.charCodeAt(i);
- 
-          if (code >= 65 && code <= 90) {
-         c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-       }
- 
-            else if (code >= 97 && code <= 122) {
-         c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
-       }
-     }
- 
-        output += c;
-   }
- 
-     return output;
- };
-
- const cipher = {encode
- };
 export default cipher;
 
--- src/index.js
+++ src/index.js
@@ -1,30 +1,19 @@
 import cipher from './cipher.js';
 console.log(cipher);
 
-const textInput = document.getElementById("palavra")
 document.getElementById("submit").addEventListener("click",clickCifrar)
-textInput.addEventListener ("submit",function(){
 
-  if(!CheckChar) {
-    e.preventDefault
-      }
-});
-function CheckChar() {
-const char = String.fromCharCode;
-const pattern = "[a-zA-Z]";
-if (char.match(pattern))
-return true;
-}
 function clickCifrar() {
-var palavra = document.getElementById("palavra").value 
-var deslocamento = parseInt(document.getElementById("deslocamento").value);
-var cifra = cipher.encode(palavra,deslocamento);
-document.getElementById("resposta").value = cifra;
-if (palavra ===""){
-window.alert("Escreva uma palavra")
+    var palavra = document.getElementById("palavra").value 
+    var deslocamento = document.getElementById("deslocamento");
+    var cifra = cipher.encode(palavra,parseInt(deslocamento.value));
+    document.getElementById("resposta").value = cifra;
+    console.log(palavra,typeof deslocamento.value)
+    if (palavra ===""){
+        window.alert("Escreva uma palavra")
+    }
 }
-}
-//document.getElementById("cifrar").addEventListener("click",clickCifrar);
+
 
 
 
