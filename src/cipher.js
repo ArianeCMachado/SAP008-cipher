ee6eb39ca6d10e451e1bb22b8d3c3359c702d08f
ArianeCMachado <109089136+ArianeCMachado@users.noreply.github.com>
2022-08-09 12:57:08 -0300

    Finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,20 @@
 const encode = function (deslocamento, palavra) {
-   
-    if (typeof palavra != "string" || palavra == "number")
-    throw new TypeError(["Precisa escolher um deslocamento"])
-
+    if (typeof palavra != "string" || typeof deslocamento != "number"){
+        throw new TypeError("Palavra ou deslocamento inválidos")
+    }
+    if (palavra == ""){
+        throw new TypeError("Digite alguma mensagem")
+    }
     var output = "";
     for (var i = 0; i < palavra.length; i++) {
         var atual = palavra[i];
-              var code = palavra.charCodeAt(i);
-              if (code >= 65 && code <= 90) {
+        var code = palavra.charCodeAt(i);
+        if (code >= 65 && code <= 90) {
             atual = String.fromCharCode(((code - 65 + deslocamento) % 26) + 65);
         }
-            else if (code >= 97 && code <= 122) {
+        else if (code >= 97 && code <= 122) {
             atual = String.fromCharCode(((code - 97 + deslocamento) % 26) + 97);
-            }
-            else if (code >= 32 && code <= 64) {
-                atual = String.fromCharCode(code);
-                }
+        }
 
         output += atual;
     }
@@ -30,17 +29,14 @@ const decode = function (deslocamento1, palavra1) {
     var output1 = "";
     for (var i1 = 0; i1 < palavra1.length; i1++) {
         var atual1 = palavra1[i1];
-              var code1 = palavra1.charCodeAt(i1);
-              if (code1 >= 65 && code1 <= 90) {
-               atual1 = String.fromCharCode(((code1 - 90 - deslocamento1) % 26) + 90);
-            }   
+        var code1 = palavra1.charCodeAt(i1);
+        if (code1 >= 65 && code1 <= 90) {
+            atual1 = String.fromCharCode(((code1 - 90 - deslocamento1) % 26) + 90);
+        }   
         
-               else if (code1 >= 97 && code1 <= 122) {
-               atual1 = String.fromCharCode(((code1 - 122 - deslocamento1) % 26) + 122);
-                }
-                else if (code1 >= 32 && code1 <= 64) {
-                atual1 = String.fromCharCode(code1);
-                }
+        else if (code1 >= 97 && code1 <= 122) {
+            atual1 = String.fromCharCode(((code1 - 122 - deslocamento1) % 26) + 122);
+        }
                            
         output1 += atual1;
     }
--- src/index.js
+++ src/index.js
@@ -4,24 +4,27 @@ document.getElementById("submit").addEventListener("click",clickCifrar)
 
 function clickCifrar() {
     var palavra = document.getElementById("palavra").value 
-    var deslocamento = document.getElementById("deslocamento");
-    var cifra = cipher.encode(parseInt(deslocamento.value), palavra);
-    document.getElementById("resposta").value = cifra;
-    if (palavra ===""){
-        window.alert("Escreva uma palavra")
+    var deslocamento = document.getElementById("deslocamento").valueAsNumber;
+    try{
+        var cifra = cipher.encode(deslocamento, palavra);
+        document.getElementById("resposta").value = cifra;
     }
- 
+    catch(error){
+        alert(error.message)
+    }
 }
-
 
 document.getElementById("submit1").addEventListener("click",clickCifrar1)
 
 function clickCifrar1() {
     var palavra1 = document.getElementById("palavra1").value 
-    var deslocamento1 = document.getElementById("deslocamento1");
-    var cifra1 = cipher.decode(parseInt(deslocamento1.value), palavra1);
-    document.getElementById("resposta1").value = cifra1;
-    if (palavra1 ===""){
-        window.alert("Escreva uma palavra")
-}
+    var deslocamento1 = document.getElementById("deslocamento1").valueAsNumber;
+    try{
+        var cifra1 = cipher.decode(deslocamento1, palavra1);
+        document.getElementById("resposta1").value = cifra1;
+    }
+    catch(error){
+        alert(error.message)
+    }
+
 }
